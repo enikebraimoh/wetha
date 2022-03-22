296714d089911a6281bb763d51f3b77551da515a
enikebraimoh <richardbraimoh@gmail.com>
2022-03-22 16:02:26 +0100

    added hilt

--- app/src/main/java/com/enike/wetha/MainActivity.kt
+++ app/src/main/java/com/enike/wetha/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.enike.wetha.ui.theme.WethaTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
