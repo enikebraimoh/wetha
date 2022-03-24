cf12a424e778985439fa43cf5e6c23a0bd1c3945
enikebraimoh <richardbraimoh@gmail.com>
2022-03-24 22:51:10 +0100

    changed Fonts 🥳

--- app/src/main/java/com/enike/wetha/ui/theme/Type.kt
+++ app/src/main/java/com/enike/wetha/ui/theme/Type.kt
@@ -2,17 +2,31 @@ package com.enike.wetha.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.enike.wetha.R
 
 // Set of Material typography styles to start with
+
+private val CircularStd = FontFamily(
+    Font(R.font.circular_std_book, FontWeight.Light),
+    Font(R.font.circular_std_bold, FontWeight.Bold)
+)
+
 val Typography = Typography(
+    h4 = TextStyle(
+        fontFamily = CircularStd,
+        fontWeight = FontWeight.Bold,
+        fontSize = 30.sp
+    ),
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
+        fontFamily = CircularStd,
+        fontWeight = FontWeight.Light,
+        fontSize = 20.sp
     )
+
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
